8d683b76b01d2fad95a0976ea73d6bbcdd07604a
Krzysztof Blazewicz <121895984+Turrz@users.noreply.github.com>
2023-09-19 14:52:05 +0100

    Add files via upload

---
+++ QuizGame/scripts/quizgame.js
@@ -0,0 +1,31 @@
+// Quiz Main 
+
+funtion 
+
+
+
+
+
+
+
+
+
+// Question 1 
+
+// Question 2 
+
+// Question 3 
+
+// Question 4
+
+// Question 5
+
+// Question 6
+
+// Question 7
+
+// Question 8 
+
+// Question 9 
+
+// Question 10 
